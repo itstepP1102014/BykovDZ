f480df08a684eb2cbdf829199401f8f2f749411c
dmec <danik.bykov@mail.ru>
2014-12-08 14:57:44 +0300

    stikh

--- programDZ/stikh/main.c
+++ programDZ/stikh/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("And The Moon And The Stars And The World"
+"by Charles Bukowski"
+"Long walks at night--"
+"that’s what good for the soul:"
+"peeking into windows"
+"watching tired housewives"
+"trying to fight off"
+"their beer-maddened husbands.\n");
     return 0;
 }
