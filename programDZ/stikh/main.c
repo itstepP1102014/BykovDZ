292877e110062c4087ded1f62bffef9f9c1c1221
dmec <danik.bykov@mail.ru>
2014-12-09 12:37:15 +0300

    stikh redaktor

--- programDZ/stikh/main.c
+++ programDZ/stikh/main.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    printf("And The Moon And The Stars And The World/n
-by Charles Bukowski/n
-Long walks at night--/n
-that’s what good for the soul:/n
-peeking into windows/n
-watching tired housewives/n
-trying to fight off/n
+    printf("And The Moon And The Stars And The World\n;
+by Charles Bukowski\n;
+Long walks at night--\n;
+that’s what good for the soul:\n;
+peeking into windows\n;
+watching tired housewives\n;
+trying to fight off\n;
 their beer-maddened husbands.\n");
     return 0;
 }
