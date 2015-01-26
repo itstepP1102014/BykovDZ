afce44dd0f648545143938e85e0b813db968e17f
dmec <danik.bykov@mail.ru>
2015-01-26 13:41:17 +0000

    Dz004 T3

---
+++ programDZ/Dz004/T3/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Variant I
+/*
+int main()
+{
+    for(int i=1; i<=33; ++i)
+    {
+        if(i%2==1)
+        {
+            printf("%d --- %d\n",i,i*i*i);
+        }
+    }
+    return 0;
+}
+//*/
+
+// Variant II
+/*
+int main()
+{
+    for(int i = 1; i <= 33; i = i + 2)
+    {
+        printf("%d --- %d\n", i, i*i*i);
+    }
+    return 0;
+}
+//*/
+
+// Variant III
+//*
+int main()
+{
+    for(int i = 1, x = 1; i <= 17; ++i, x = x + 2)
+    {
+        printf("%d: %d --- %d\n", i, x, x*x*x);
+    }
+    return 0;
+}
+//*/
