aef68a99417bbade97cf8fc594c20fd5860ef52c
dmec <danik.bykov@mail.ru>
2015-01-28 12:51:42 +0000

    T4Dz4

---
+++ programDZ/Dz004/T4/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int s;
+    int n;
+    printf("Ведите число: ");
+    scanf(" %d\n", &n);
+    printf("Ведите число: ");
+    scanf(" %d\n", &s);
+    for(int a,k; a=s; k=n*a ++i)
+        if(n<=???)
+    {
+        printf("$d*$d=%d\n", n*s=k);
+    }
+    return 0;
+}
