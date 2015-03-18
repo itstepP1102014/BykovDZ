952915c0f4e4fc613a99b8cd7b941de70dc58153
dmec <danik_bykov@mail.ru>
2015-03-18 13:30:05 +0300

    T3

---
+++ programDZ/Dz005/T3/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int min;
+    int makc;
+    int x;
+    int n;
+    printf("n: ");
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i)
+    {
+        printf("i: ");
+        scanf("%d", &i);
+
+    }
+    printf("n=%d", n);
+    return 0;
+}
